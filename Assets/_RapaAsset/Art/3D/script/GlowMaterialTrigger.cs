4f5b026f4255e1bbf74fa8c98a96a0abc80d7005
frogskin <cyshenfrog@gmail.com>
2022-12-27 09:59:43 +0800

    3D

---
+++ Assets/_RapaAsset/Art/3D/script/GlowMaterialTrigger.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowMaterialTrigger : MonoBehaviour
+{
+    public Shader targetShader;
+    private Renderer rend;
+    private MaterialPropertyBlock mpb;
+    private int count;
+    private string[] v3Names = new string[5] { "_position", "_position1", "_position2", "_position3", "_position4" };
+    private int delayCounter;
+
+    private void Start()
+    {
+        mpb = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        if (!rend)
+            return;
+        if (rend.material.shader != targetShader)
+            return;
+        delayCounter++;
+        if (delayCounter >= 4)
+        {
+            WritePosToMaterial();
+            delayCounter = 0;
+        }
+    }
+
+    private void WritePosToMaterial()
+    {
+        mpb.SetVector(v3Names[count], transform.position);
+        rend.SetPropertyBlock(mpb);
+        count++;
+        if (count >= v3Names.Length)
+            count = 0;
+    }
+
+    private void ClearMatVector()
+    {
+        if (!rend)
+            return;
+        foreach (var name in v3Names)
+        {
+            mpb.SetVector(name, Vector3.zero);
+            rend.SetPropertyBlock(mpb);
+        }
+        rend = null;
+    }
+
+    public void SetTarget(Component target)
+    {
+        if (!target)
+        {
+            ClearMatVector();
+            return;
+        }
+        if (rend == target.GetComponent<Renderer>())
+            return;
+        else
+            ClearMatVector();
+
+        rend = target.GetComponent<Renderer>();
+        if (!rend)
+            return;
+        if (rend.material.shader != targetShader)
+            return;
+        count = 0;
+        mpb = new MaterialPropertyBlock();
+    }
+
+    public void SpawnGuideFX()
+    {
+        //PrototypeMain.Instance.SpawnGuideFX(transform);
+    }
+}
---
+++ Assets/_RapaAsset/Art/3D/script/SeedGrowing.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedGrowing : MonoBehaviour
+{
+    SkinnedMeshRenderer SkinnedMeshRenderer;
+    Mesh skinnedMesh;
+    float growingValue = 0f;
+
+    float cutOffValue = 2f;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        SkinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
+    }
+    void Start()
+    {
+      
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        growingValue += Time.deltaTime * 40f;
+        Debug.Log(growingValue);
+        if (growingValue > 250)
+        {
+            return;
+        }
+        else
+        {
+            if (growingValue < 100)
+            {
+                SkinnedMeshRenderer.SetBlendShapeWeight(0, growingValue);
+
+            }
+            else
+            {
+                SkinnedMeshRenderer.SetBlendShapeWeight(0, 200f - growingValue);
+                SkinnedMeshRenderer.SetBlendShapeWeight(1, growingValue - 100f);
+                cutOffValue -= Time.deltaTime * 0.6f;
+                this.GetComponent<Renderer>().material.SetFloat("_CutoffHeight", cutOffValue);
+            }
+
+        }
+
+       
+
+
+    }
+}
